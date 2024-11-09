609eafb672e112c4654e741e004d9714a1cb5de5
Jessica Cousins <jcousins87@gmail.com>
2024-11-08 20:08:20 -0500

    navbar styles and layout updated

--- frontend/src/components/Layout/NavBar.jsx
+++ frontend/src/components/Layout/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { auth } from "../../../firebase";
 import { useAuth } from "../../context/AuthContext";
-import { FaBars, FaTimes, FaShoppingCart } from "react-icons/fa";
+import { FaBars, FaTimes, FaShoppingCart, FaSearch } from "react-icons/fa";
 import "./NavBar.css";
 
 const NavBar = ({ onSearch, cartItemCount }) => {
@@ -34,97 +34,73 @@ const NavBar = ({ onSearch, cartItemCount }) => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
-  const handleSearch = (event) => {
-    if (event.key === "Enter") {
-      onSearch(event.target.value);
-    }
-  };
-
   return (
-    <nav className="navbar" ref={navRef}>
-      <div className="navbar-header">
-        <h1 className="navbar-brand">Logo Name</h1>
-        <button className="hamburger-menu" onClick={toggleMobileMenu}>
-          {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
-        </button>
-      </div>
-
-      <ul className={`navbar-list ${isMobileMenuOpen ? "open" : ""}`}>
-        {!user ? (
-          <div className="navbar-auth-links">
-            <li onClick={closeMobileMenu}>
-              <NavLink
-                to="/login"
-                className={({ isActive }) => (isActive ? "active-link" : "")}
-              >
-                Login
-              </NavLink>
-            </li>
-            <li onClick={closeMobileMenu}>
-              <NavLink
-                to="/signup"
-                className={({ isActive }) => (isActive ? "active-link" : "")}
-              >
-                SignUp
-              </NavLink>
-            </li>
-          </div>
-        ) : (
-          <>
-            {[
-              { path: "/home", label: "Home" },
-              { path: "/about", label: "News" },
-              { path: "/profile", label: "Profile" },
-              { path: "/blog", label: "Blog" },
-              { path: "/games", label: "Games" },
-              { path: "/checkout", label: "Checkout" },
-              { path: "/shopping", label: "Shopping" },
-              // { path: "/cart", label: "Cart" },
-              { path: "/admin-dashboard", label: "Admin" },
-            ].map((link) => (
-              <li key={link.path} onClick={closeMobileMenu}>
+    <>
+      <nav className="navbar dark-mode" ref={navRef}>
+        <div className="navbar-header">
+          <h1 className="navbar-brand">Logo Name</h1>
+          <button className="hamburger-menu" onClick={toggleMobileMenu}>
+            {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
+        <ul className={`navbar-list ${isMobileMenuOpen ? "open" : ""}`}>
+          {user && (
+            <>
+              {[
+                { path: "/home", label: "Home" },
+                { path: "/about", label: "News" },
+                { path: "/profile", label: "Profile" },
+                { path: "/blog", label: "Blog" },
+                { path: "/games", label: "Games" },
+                { path: "/checkout", label: "Checkout" },
+                { path: "/shopping", label: "Shopping" },
+                { path: "/admin-dashboard", label: "Admin" },
+              ].map((link) => (
+                <li key={link.path} onClick={closeMobileMenu}>
+                  <NavLink
+                    to={link.path}
+                    className={({ isActive }) =>
+                      isActive ? "active-link" : ""
+                    }
+                  >
+                    {link.label}
+                  </NavLink>
+                </li>
+              ))}
+              <li className="cart-link" onClick={closeMobileMenu}>
                 <NavLink
-                  to={link.path}
+                  title="Cart"
+                  to="/cart"
                   className={({ isActive }) => (isActive ? "active-link" : "")}
                 >
-                  {link.label}
+                  <FaShoppingCart />
+                  {cartItemCount > 0 && (
+                    <span className="cart-count">{cartItemCount}</span>
+                  )}
                 </NavLink>
               </li>
-            ))}
-            <li className="cart-link" onClick={closeMobileMenu}>
-              <NavLink
-                title="Cart"
-                to="/cart"
-                className={({ isActive }) => (isActive ? "active-link" : "")}
-              >
-                <FaShoppingCart />
-                {cartItemCount > 0 && (
-                  <span className="cart-count">{cartItemCount}</span>
-                )}
-              </NavLink>
-            </li>
-            <li>
-              <div className="search-container" title="Search">
-                <input
-                  type="text"
-                  placeholder="Search Blog Posts..."
-                  onChange={handleSearchChange}
-                  className="search-input"
-                />
-                <button className="search-button" onClick={closeMobileMenu}>
-                  🔍
+              <li>
+                <button onClick={handleLogout} className="logout-button">
+                  Logout
                 </button>
-              </div>
-            </li>
-            <li>
-              <button onClick={handleLogout} className="logout-button">
-                Logout
-              </button>
-            </li>
-          </>
-        )}
-      </ul>
-    </nav>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+
+      <div className="search-bar sticky-search-bar">
+        <input
+          type="text"
+          placeholder="Search Blog Posts..."
+          onChange={handleSearchChange}
+          className="search-input"
+        />
+        <button className="search-button">
+          <FaSearch />
+        </button>
+      </div>
+    </>
   );
 };
 
